1f71d870ce77ccccb8ff37a90ccc3900e8ed9e61
Cx1Ang <1002796232@qq.com>
2023-12-27 22:30:18 +0800

    复制静态文件

---
+++ generate-Code-basic/src/main/java/com/cheng/generator/StaticGenerator.java
@@ -0,0 +1,33 @@
+package com.cheng.generator;
+
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
+public class StaticGenerator {
+    public static void main(String[] args) {
+        //获取整个项目的根目录(basic)
+//        D:\code\generate-Code\generate-Code-basic
+        String projectPath = System.getProperty("user.dir");
+
+        //获取父项目的路径(gene-Code)
+        File projecttFile = new File(projectPath).getParentFile();
+
+        //输入路径 ACM程序路径
+//        D:\code\generate-Code\generate-Code-demo-project\acm-template
+        String srcPath = new File(projecttFile, "generate-Code-demo-project/acm-template").getAbsolutePath();
+
+        copyFileByHutool(srcPath, projectPath);
+    }
+
+    /**
+     * Params:
+     * @param srcPath 源文件或目录
+     * @param destPath 目标文件或目录，目标不存在会自动创建（目录、文件都创建）
+     * isOverride – 是否覆盖目标文件
+     */
+    public static void copyFileByHutool(String srcPath, String destPath) {
+        FileUtil.copy(srcPath, destPath, false);
+    }
+
+}
